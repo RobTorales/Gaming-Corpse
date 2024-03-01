8fdad1f6707c347b29fa240f65ed0e2ead4b154b
roberto <roberto1608torales@gmail.com>
2024-03-01 13:42:51 -0300

    ok

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = () => {
 
 <nav className="navbar navbar-expand-lg bg-white">
   <div className="container-fluid d-flex">
-  <Link className="navbar-brand" to="/"><img className="img-logo"src="/img/logo-gaming.png" alt="Gaming Corpse" /></Link>
+  <Link className="navbar-brand" to="/"><img className="img-logo img-fluid"src="/img/logo-gaming.png" alt="Gaming Corpse" /></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
